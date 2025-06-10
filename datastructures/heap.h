74cb6d81b0a4935df60dfb626644c7098e083fb2
Florian ten Napel <tennapel.f@gmail.com>
2025-06-10 16:37:22 +0200

    changed api for simpler usage

--- datastructures/heap.h
+++ datastructures/heap.h
@@ -4,6 +4,8 @@
 #include <assert.h>
 #include <stddef.h>
 
+#include "vec.h"
+
 #define _HEAP_LEFT(i) ((i) * 2 + 1)
 #define _HEAP_RIGHT(i) ((i) * 2 + 2)
 #define _HEAP_PARENT(i) (((i)-1) / 2)
@@ -12,20 +14,29 @@
 * Creates functions for a min-heap, dependent on a base Vector type
 *
 * @param HEAP_NAME prefix of function names
-* @param HEAP_VECTOR_TYPE name of underlying Vector type
-* @param HEAP_VAL_TYPE type of values stored in Vector, must be same as Vector value type
+* @param HEAP_VAL_TYPE type of values stored in heap
 * @param HEAP_VAL_CMP_FUNC int (*)(const <HEAP_VAL_TYPE>* a, const <HEAP_VAL_TYPE>* b)
 *   function or macro to compare two entries. 
 *   Given two entries a and b, should return less than 0 if a < b, 
 *   more than 0 if a > b and 0 if a == b
 *   If a max heap is desired, this function should return the opposite ordering
 ****************************************************************************************/
-#define HEAP_DEFINE(HEAP_NAME, HEAP_VECTOR_TYPE, HEAP_VAL_TYPE, HEAP_VAL_CMP_FUNC) \
+#define HEAP_DEFINE(HEAP_NAME, HEAP_VAL_TYPE, HEAP_VAL_CMP_FUNC) \
+    VEC_DEFINE(_##HEAP_NAME##_VECTOR_TYPE, HEAP_VAL_TYPE) \
+    typedef _##HEAP_NAME##_VECTOR_TYPE HEAP_NAME; \
+    \
+    /**************************
+     * Returns a new min-heap
+     **************************/ \
+    HEAP_NAME HEAP_NAME##_new() \
+    { \
+        return _##HEAP_NAME##_VECTOR_TYPE##_new(0); \
+    } \
     /*******************************************
      * Sifts the element at a given index down 
      * until the heap invariant is restored
      *******************************************/ \
-    void _##HEAP_NAME##_sift_down(HEAP_VECTOR_TYPE* vector, size_t i) \
+    void _##HEAP_NAME##_sift_down(HEAP_NAME* vector, size_t i) \
     { \
         size_t left = _HEAP_LEFT(i), right = _HEAP_RIGHT(i), min_child_ind; \
         while (left < vector->size) { \
@@ -53,12 +64,16 @@
     \
     /**********************************************
     * Constructs a min-heap from the given vector
+    *
+    * @param vector Can have any vector type with 
+    *   identical <VALUE_TYPE> as the heap
     ***********************************************/ \
-    void HEAP_NAME##_heapify(HEAP_VECTOR_TYPE* vector) \
+    void HEAP_NAME##_heapify(void* vector) \
     { \
-        size_t i = _HEAP_PARENT((vector->size - 1)); \
+        _##HEAP_NAME##_VECTOR_TYPE* cast_vec = (_##HEAP_NAME##_VECTOR_TYPE*) vector; \
+        size_t i = _HEAP_PARENT((cast_vec->size - 1)); \
         do { \
-            _##HEAP_NAME##_sift_down(vector, i); \
+            _##HEAP_NAME##_sift_down(cast_vec, i); \
         } while (i--); \
     } \
     \
@@ -66,10 +81,10 @@
     /************************************
      * Inserts an element into the Heap
      ************************************/ \
-    void HEAP_NAME##_push(HEAP_VECTOR_TYPE* vector, HEAP_VAL_TYPE value) \
+    void HEAP_NAME##_push(HEAP_NAME* vector, HEAP_VAL_TYPE value) \
     { \
         assert(vector); \
-        HEAP_VECTOR_TYPE##_push(vector, value); \
+        _##HEAP_NAME##_VECTOR_TYPE##_push(vector, value); \
         size_t ind = vector->size-1; \
         size_t par_ind = _HEAP_PARENT(ind); \
         while (ind > 0 && (HEAP_VAL_CMP_FUNC(((const HEAP_VAL_TYPE*)(vector->arr+ind)), ((const HEAP_VAL_TYPE*)(vector->arr+par_ind)))) < 0) { \
@@ -85,13 +100,21 @@
     /************************************************************
      * Removes the minimum element from the Heap and returns it
      ************************************************************/ \
-    HEAP_VAL_TYPE HEAP_NAME##_pop(HEAP_VECTOR_TYPE* vector) \
+    HEAP_VAL_TYPE HEAP_NAME##_pop(HEAP_NAME* vector) \
     { \
         assert(vector); \
         HEAP_VAL_TYPE ret = vector->arr[0]; \
-        vector->arr[0] = HEAP_VECTOR_TYPE##_pop(vector); \
+        vector->arr[0] = _##HEAP_NAME##_VECTOR_TYPE##_pop(vector); \
         _##HEAP_NAME##_sift_down(vector, 0); \
         return ret; \
+    } \
+    \
+    /**************************************
+     * deallocates resources used by heap
+     **************************************/ \
+    void HEAP_NAME##_free(HEAP_NAME* heap) \
+    { \
+        _##HEAP_NAME##_VECTOR_TYPE_free(heap); \
     }
 
 #endif
